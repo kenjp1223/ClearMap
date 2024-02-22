0290df82094778b01b29c3c6d4a8760d76b355ab
kenjp1223 <kenjp1223@gmail.com>
2024-02-22 13:54:22 -0800

    Update Settings.py
    get Ilastik/Elastix default file ready

--- ClearMap/Settings.py
+++ ClearMap/Settings.py
@@ -16,7 +16,7 @@ import os
 import socket
 
 #path to Ilastik installation
-IlastikPath = '.../ClearMap/ClearMap/external/ilastik-1.4.0rc6-Linux';
+IlastikPath = '/ilastik-1.3.3post3-Linux';
 """str: Absolute path to the Ilastik 0.5 installation
 
 Notes:
@@ -26,7 +26,7 @@ Notes:
 """
 
 #path to elastix installation
-ElastixPath = '.../ClearMap/ClearMap/external/elastix_linux64_v4.8';
+ElastixPath = '/elastix_linux64_v4.8';
 """str: Absolue path to the elastix installation
 
 Notes:
